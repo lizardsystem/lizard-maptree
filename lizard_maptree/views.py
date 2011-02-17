881a3d48bc27ec7890108650cde3c314a5ad990c
jackha <jackha@gmail.com>
2011-02-17 16:19:13 +0000

    unfinished

--- lizard_maptree/views.py
+++ lizard_maptree/views.py
@@ -13,12 +13,21 @@ from lizard_maptree.models import Category
 
 def homepage(request,
              root_slug=None,
-             item_classes=None,
+             item_models=None,
              javascript_click_handler='popup_click_handler',
              javascript_hover_handler='popup_hover_handler',
              template="lizard_maptree/homepage.html"):
 
-    def treeitems(item_classes=None):
+    """
+    item_models: i.e. ['wmssource', ] (in lower case!) ->
+       category.__getattribute__('wmssource_set').all()
+
+       These models must implement the 'workspace_acceptable'
+       function, the function returns a dict with keys name, type,
+       description, adapter_layer_json, adapter_name.
+    """
+
+    def treeitems(category, item_models=None):
         """
         result = [{'name': 'testname',
         'type': 'workspace-acceptable',
@@ -26,16 +35,22 @@ def homepage(request,
         'adapter_layer_json': '"json"',
         'adapter_name': 'adapter'},]
         """
-        if item_classes is None:
+        result = []
+        if item_models is None:
             # Add dummy
             result = [{'name': 'dummy workspace-acceptable',
                        'type': 'workspace-acceptable',
                        'description': 'description',
                        'adapter_layer_json': '"json"',
                        'adapter_name': 'adapter'},]
+            return result
+        for item_model_name in item_models:
+            item_model_set = '%s_set' % item_model_name
+            for item in category.__getattribute__(item_model_set).all():
+                result.append(item.workspace_acceptable())
         return result
 
-    def get_tree(parent=None, item_classes=None):
+    def get_tree(parent=None, item_models=None):
         """
         Make tree for homepage using Category and Shape.
         """
@@ -43,19 +58,20 @@ def homepage(request,
         categories = Category.objects.filter(parent=parent)
         for category in categories:
             # Append sub categories.
-            children = get_tree(parent=category)
+            children = get_tree(parent=category, item_models=item_models)
             row = {'name': category.name,
                    'type': 'category',
                    'children': children}
             result.append(row)
+        # Append workspace-acceptables.
         if parent is not None:
-            result += treeitems(item_classes=item_classes)
+            result += treeitems(parent, item_models=item_models)
         return result
 
     parent_category = None
     if root_slug is not None:
         parent_category = get_object_or_404(Category, slug=root_slug)
-    tree = get_tree(parent_category, item_classes)
+    tree = get_tree(parent_category, item_models)
 
     workspace_manager = WorkspaceManager(request)
     workspaces = workspace_manager.load_or_create()
