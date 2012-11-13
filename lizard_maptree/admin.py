d500f815c0b96f351c1267fd862e3d6cb6275df9
Reinout van Rees <reinout@vanrees.org>
2012-11-13 12:22:22 +0100

    pep8

--- lizard_maptree/admin.py
+++ lizard_maptree/admin.py
@@ -3,25 +3,33 @@ from django import forms
 
 from lizard_maptree.models import Category
 
+
 def category_ancestors(obj):
     return ' -> '.join(['root'] + [a.slug for a in obj.get_ancestors()])
 
+
 def category_descendants(obj):
     return ', '.join([a.name for a in obj.get_descendants()])
+
 
 class ParentCategoryField(forms.ModelChoiceField):
     def label_from_instance(self, obj):
         return "%s (%s)" % (obj.name, obj.slug)
 
+
 class CategoryForm(forms.ModelForm):
-    parent = ParentCategoryField(queryset=Category.objects.all(), required=False)
+    parent = ParentCategoryField(queryset=Category.objects.all(),
+                                 required=False)
+
     class Meta:
         model = Category
+
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('__unicode__', 'slug', category_ancestors,
                     category_descendants)
     list_filter = ('name', 'parent', 'slug', )
     form = CategoryForm
+
 
 admin.site.register(Category, CategoryAdmin)
