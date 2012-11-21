e4ca530a8ebac5aa5d159a380842b2d790ea5c62
Sander Smits <sander.smits@nelen-schuurmans.nl>
2012-11-21 15:07:50 +0100

    PEP8 changes

--- lizard_maptree/tests/models.py
+++ lizard_maptree/tests/models.py
@@ -16,9 +16,12 @@ class CategoryTests(TestCase):
     def test_index_based_sorting(self):
         """Tests index based category sorting"""
         parent_1 = CategoryFactory.create(name="parent 1", index=10)
-        sub_1 = CategoryFactory.create(name="sub 1", index=13, parent=parent_1)
-        sub_2 = CategoryFactory.create(name="sub 2", index=12, parent=parent_1)
-        sub_3 = CategoryFactory.create(name="sub 3", index=11, parent=parent_1)
+        sub_1 = CategoryFactory.create(name="sub 1", index=13,
+            parent=parent_1)
+        sub_2 = CategoryFactory.create(name="sub 2", index=12,
+            parent=parent_1)
+        sub_3 = CategoryFactory.create(name="sub 3", index=11,
+            parent=parent_1)
         categories = Category.objects.all()
         indexes = []
         for category in categories:
@@ -30,13 +33,19 @@ class CategoryTests(TestCase):
     def test_index_based_sorting_with_two_parents(self):
         """Tests index based category sorting with two parents"""
         parent_2 = CategoryFactory.create(name="parent 2", index=1)
-        sub_1a = CategoryFactory.create(name="sub 1a", index=2, parent=parent_2)
-        sub_2a = CategoryFactory.create(name="sub 2a", index=4, parent=parent_2)
-        sub_3a = CategoryFactory.create(name="sub 3a", index=3, parent=parent_2)
+        sub_1a = CategoryFactory.create(name="sub 1a", index=2,
+            parent=parent_2)
+        sub_2a = CategoryFactory.create(name="sub 2a", index=4,
+            parent=parent_2)
+        sub_3a = CategoryFactory.create(name="sub 3a", index=3,
+            parent=parent_2)
         parent_1 = CategoryFactory.create(name="parent 1", index=10)
-        sub_1 = CategoryFactory.create(name="sub 1", index=13, parent=parent_1)
-        sub_2 = CategoryFactory.create(name="sub 2", index=12, parent=parent_1)
-        sub_3 = CategoryFactory.create(name="sub 3", index=11, parent=parent_1)
+        sub_1 = CategoryFactory.create(name="sub 1", index=13,
+            parent=parent_1)
+        sub_2 = CategoryFactory.create(name="sub 2", index=12,
+            parent=parent_1)
+        sub_3 = CategoryFactory.create(name="sub 3", index=11,
+            parent=parent_1)
         categories = Category.objects.all()
         indexes = []
         for category in categories:
